0443c67e468baaeec45c24d72ace09e28c9f4df0
wangpeiqi <LogicWang@outlook.com>
2021-01-07 22:59:14 +0800

    add leet code

--- src/main/java/person/me/epitomize/algorithm/C1.java
+++ src/main/java/person/me/epitomize/algorithm/C1.java
@@ -9,6 +9,40 @@ public class C1 {
 
     }
 
+    /**
+     *
+     * @return is exist
+     */
+    public boolean existInorderedMatrix(int n, int[][] matrix){
+
+        return  true;
+    }
+
+
+    public static int findAnyRepeat(int[] nums){
+        for (int i=0; i< nums.length; i++){
+            while (nums[i] != i){
+                if (nums[i] == nums[nums[i]]){
+                    return nums[i];
+                }
+                swap(nums, i, nums[i]);
+            }
+        }
+        return -1;
+    }
+
+    public static void  swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+
+    public static void main(String[] args) {
+        int[] input = new int[]{0,1,2,3,4,3};
+        System.out.println(findAnyRepeat(input));
+    }
+
 
 
 }
