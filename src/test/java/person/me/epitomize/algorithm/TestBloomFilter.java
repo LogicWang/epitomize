9182cdf50365a8c49c407b2e81582140821b9de2
wangpeiqi <LogicWang@outlook.com>
2021-04-19 00:18:17 +0800

    add bloom filter

---
+++ src/test/java/person/me/epitomize/algorithm/TestBloomFilter.java
@@ -0,0 +1,32 @@
+package person.me.epitomize.algorithm;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestBloomFilter {
+    @Test
+    public void testBloomFilter() {
+        BloomFilter bloomFilter = new BloomFilter(1000, 5);
+        bloomFilter.add("a");
+
+        Assert.assertFalse(bloomFilter.contain("b"));
+        Assert.assertTrue(bloomFilter.contain("a"));
+    }
+
+    @Test
+    public void testBloomFilter2() {
+        BloomFilter bloomFilter = new BloomFilter(1000, 5);
+        int l = 90;
+        for (int i = 0; i < l; i++) {
+            bloomFilter.add("a" + i);
+        }
+
+        for (int i = 0; i < l; i++) {
+            Assert.assertFalse(bloomFilter.contain("b" + i));
+        }
+
+        for (int i=0;i<l;i++) {
+            Assert.assertTrue(bloomFilter.contain("a" + i));
+        }
+    }
+}
